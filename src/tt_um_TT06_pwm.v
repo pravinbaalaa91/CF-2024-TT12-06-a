9e0aebd03964087d805ea80e664f0a6ceeebb5b8
Pravin Baalaa <pravinbaalaa91@gmail.com>
2025-08-20 16:20:37 +0530

    Update tt_um_TT06_pwm.v

--- src/tt_um_TT06_pwm.v
+++ src/tt_um_TT06_pwm.v
@@ -1,5 +1,6 @@
 
 `default_nettype none
+
 module tt_um_TT06_pwm (
     input  wire clk,
     input  wire rst_n,
@@ -38,40 +39,4 @@ module tt_um_TT06_pwm (
     assign uio_oe  = 8'b0;
     wire _unused = &{ui_in[7], uio_in[7:0], ena};
 
-endmodule
-
-
-module pwm (
-    input clk,
-    input reset,
-    input wire [6:0] dc,
-    output reg pwm_out,
-    output reg pwm_out1
-);
-    reg [7:0] count;
-    wire [7:0] threshold;
-
-    assign threshold = (dc * 255) / 100;
-
-    always @(posedge clk or negedge reset) begin
-        if (!reset) begin
-            count <= 8'd0;
-            pwm_out <= 0;
-            pwm_out1 <= 0;
-        end else begin
-            count <= count + 1;
-
-            if (threshold == 0) begin
-                pwm_out <= 0;
-            end else if (dc >= 7'd100) begin
-                pwm_out <= 1;
-            end else if (count <= threshold) begin
-                pwm_out <= 1;
-            end else begin
-                pwm_out <= 0;
-            end
-
-            pwm_out1 <= pwm_out;
-        end
-    end
 endmodule
