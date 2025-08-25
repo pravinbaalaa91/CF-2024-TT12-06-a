5790ca96ada516cee138807ab581c9393a0f6565
Pravin Baalaa <pravinbaalaa91@gmail.com>
2025-08-25 11:15:12 +0530

    Delete test/test.py

--- test/test.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
-import cocotb
-
-@cocotb.test()
-async def test_project(dut):
-    dut._log.info("Dummy test – no checks, always passes")
-    await cocotb.triggers.Timer(1, units="us")
